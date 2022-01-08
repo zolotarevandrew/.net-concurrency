f1c7329de803658e9e29978b27afaabc7f1794f5
Andrew Zolotarev <gagasusrock@gmail.com>
2022-01-08 14:38:16 +0300

    init repo

---
+++ AsyncPromise/AsyncPromiseBuilder.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.CompilerServices;
+using System.Runtime.ExceptionServices;
+
+namespace AsyncPromise
+{
+    public class AsyncPromiseBuilder<T>
+    {
+        public static AsyncPromiseBuilder<T> Create()
+        {
+            return new AsyncPromiseBuilder<T>();
+        }
+
+        public void Start<TStateMachine>(ref TStateMachine stateMachine)
+            where TStateMachine : IAsyncStateMachine
+        {
+            stateMachine.MoveNext();
+        }
+
+        public void SetStateMachine(IAsyncStateMachine stateMachine)
+        {
+            
+        }
+
+        public void SetException(Exception exception)
+        {
+            ExceptionDispatchInfo.Capture(exception).Throw();
+        }
+
+        public void SetResult(T result)
+        {
+            Task.SetResult(result);
+        }
+
+        public void AwaitOnCompleted<TAwaiter, TStateMachine>(
+            ref TAwaiter awaiter, ref TStateMachine stateMachine)
+            where TAwaiter : INotifyCompletion
+            where TStateMachine : IAsyncStateMachine
+        {
+            awaiter.OnCompleted(stateMachine.MoveNext);
+        }
+
+        public void AwaitUnsafeOnCompleted<TAwaiter, TStateMachine>(ref TAwaiter awaiter, ref TStateMachine stateMachine)
+            where TAwaiter : ICriticalNotifyCompletion
+            where TStateMachine : IAsyncStateMachine
+        {
+            awaiter.OnCompleted(stateMachine.MoveNext);
+        }
+
+        public AsyncPromise<T> Task { get; } = new AsyncPromise<T>();
+    }
+}
